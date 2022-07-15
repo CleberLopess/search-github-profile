ed5e633c6968a015a05d9932cddcb74d7aa8a343
Cleber Lopes <cleberlopes777@gmail.com>
2022-07-14 22:12:46 -0300

    create deplay in github

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  assetPrefix: isProd ? "/search-github-profile/" : "",
 };
